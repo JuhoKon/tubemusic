ad299ffd1959c7db40efd57fbd88808c5366d532
Juho Kontiainen - laptop <kontiainen97@hotmail.fi>
2020-01-22 11:52:42 +0200

    Revert "Something"
    This reverts commit ed8049c43c83703cd5618662ea8e20bf29e0d7a8.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import logo from "./logo.svg";
 import Navbar from "./components/navbar/navbar-component";
 import "./App.css";
 import HomePage from "./components/homepage/homepage-component";
