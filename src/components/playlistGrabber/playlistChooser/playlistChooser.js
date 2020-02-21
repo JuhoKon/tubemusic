f3c685928d3f945c2775327e9703602a659ec178
JuhoKon <kontiainen97@hotmail.fi>
2020-02-21 21:16:45 +0200

    Planning

---
+++ src/components/playlistGrabber/playlistChooser/playlistChooser.js
@@ -0,0 +1,6 @@
+//TODO:
+//itemit tietenkin:
+// näihin itemeihi tulee owner, private ? eli yhteissoittolista, DATE !! biisin aika, title :)
+//Ja pystyy tekemään copyn siu omalle accountille / subscribettämään yhteissoittolistaan (HUOM! Kaikki voivat
+//                                                                                        editoida näitä listoja)!
+//tälläne :)
