840ebcb65becbdc9e3ab2d6cb7750f041ecf6c9f
JuhoKon <kontiainen97@hotmail.fi>
2020-02-21 21:21:26 +0200

    Planning

--- src/components/playlistGrabber/playlistChooser/playlistChooser.js
+++ src/components/playlistGrabber/playlistChooser/playlistChooser.js
@@ -4,3 +4,9 @@
 //Ja pystyy tekemään copyn siu omalle accountille / subscribettämään yhteissoittolistaan (HUOM! Kaikki voivat
 //                                                                                        editoida näitä listoja)!
 //tälläne :)
+//eli react virtualized lista-lo -> itemit (ei voi muoakta itemeitä tässä, vaan copy koko höskä / sub)
+//Taaaaai vois tehä sellasen playlistmakerin, eli voit joko suoraan subata tossa playlists to choose from kohassa
+//sitte tässä olis kaks osane juttu, eka osa on siu tuleva playlist, oikealla puolella siu lataama playlista
+//eli voi rakentaa monesta jo olevasta playlistasta uusia playlistoja
+//filterit näihi molempiin!
+//game changing plan pog
