2c30f309655db9d54d1c198a0d96e132dfa9cf1a
JuhoKon <kontiainen97@hotmail.fi>
2020-04-22 13:29:25 +0300

    Changed response on user route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const jwtSecret = process.env.JWTSECRET;
 
-exports.index = function(req, res, next) {
+exports.index = function (req, res, next) {
   if (req.user.role !== "Admin") {
     return res
       .status(401)
@@ -12,11 +12,11 @@ exports.index = function(req, res, next) {
   }
   User.find()
     .select("name")
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json("Error: " + err));
+    .then((users) => res.json(users))
+    .catch((err) => res.status(400).json("Error: " + err));
 };
 // Handle create on POST.
-exports.create = function(req, res, next) {
+exports.create = function (req, res, next) {
   var resSent = false; //logic to prevent sending headers twice
   const email = req.body.email;
   const name = req.body.name;
@@ -27,22 +27,22 @@ exports.create = function(req, res, next) {
   }
   //Check if there's an entry with the email
   User.findOne({ email: email })
-    .then(user => {
+    .then((user) => {
       if (user) {
         if (!resSent) {
           //Some logic so we don't set headers after they are sent to client
           //in case email and username are both taken.
           resSent = true;
           return res.status(400).json({
-            error: "The email already exists. Please use a different email"
+            error: "The email already exists. Please use a different email",
           });
         }
       }
     })
-    .catch(err => res.status(400).json("Error: " + err));
+    .catch((err) => res.status(400).json("Error: " + err));
   //Check if there's an entry with the username
   User.findOne({ name: name })
-    .then(user => {
+    .then((user) => {
       if (user) {
         if (!resSent) {
           //Some logic so we don't set headers after they are sent to client
@@ -50,15 +50,15 @@ exports.create = function(req, res, next) {
           resSent = true;
           return res.status(400).json({
             error:
-              "The username already exists. Please use a different username"
+              "The username already exists. Please use a different username",
           });
         }
       }
     })
-    .catch(err => res.status(400).json("Error: " + err));
+    .catch((err) => res.status(400).json("Error: " + err));
   //Actual functionality, checks if theres a match either in email or name and creates user if not
   User.findOne({ $or: [{ email: email }, { name: name }] })
-    .then(user => {
+    .then((user) => {
       if (user) {
       } else {
         var user = new User({
@@ -66,7 +66,7 @@ exports.create = function(req, res, next) {
           email: req.body.email,
           password: req.body.password,
           age: req.body.age,
-          role: req.body.role
+          role: req.body.role,
         });
 
         // Create salt & hash
@@ -74,7 +74,7 @@ exports.create = function(req, res, next) {
           bcrypt.hash(user.password, salt, (err, hash) => {
             if (err) throw err;
             user.password = hash; //save hashed password to DB
-            user.save().then(user => {
+            user.save().then((user) => {
               jwt.sign(
                 //make token for the user
                 { id: user.id },
@@ -90,8 +90,8 @@ exports.create = function(req, res, next) {
                       name: user.name,
                       email: user.email,
                       age: user.age,
-                      role: user.role
-                    }
+                      role: user.role,
+                    },
                   });
                 }
               );
@@ -100,37 +100,37 @@ exports.create = function(req, res, next) {
         });
       }
     })
-    .catch(err => res.status(400).json("Error: " + err));
+    .catch((err) => res.status(400).json("Error: " + err));
 };
-exports.addPlaylist = function(req, res, next) {
+exports.addPlaylist = function (req, res, next) {
   if (!req.body.playlistId) {
     return res.status(400).json({
-      error: "No playlistid provided."
+      error: "No playlistid provided.",
     });
   }
   if (!req.body.playlistName) {
     return res.status(400).json({
-      error: "No playlist name provided."
+      error: "No playlist name provided.",
     });
   }
   User.findById(req.user.id)
-    .then(user => {
+    .then((user) => {
       //console.log(user);
       user.playlists.unshift({
         _id: req.body.playlistId,
         name: req.body.playlistName,
         private: req.body.private,
         owner: req.body.owner,
-        createdAt: req.body.createdAt
+        createdAt: req.body.createdAt,
       });
       user
         .save() //attempt to save the user
         .then(() => res.json(user.playlists))
-        .catch(err => res.status(400).json("Error: " + err));
+        .catch((err) => res.status(400).json("Error: " + err));
     })
-    .catch(err => res.status(400).json("Error: " + err));
+    .catch((err) => res.status(400).json("Error: " + err));
 };
-exports.removePlaylist = function(req, res, next) {
+exports.removePlaylist = function (req, res, next) {
   /* User.findById(req.user.id).then(user => {
     user.playlists.pull({ _id: "uniiekki" }).exec();
     user
@@ -141,19 +141,19 @@ exports.removePlaylist = function(req, res, next) {
   User.updateMany(
     { _id: req.user.id },
     { $pull: { playlists: { _id: req.params.id } } },
-    function(err, data) {
+    function (err, data) {
       res.json(data);
     }
   );
 };
-exports.editPlaylist = function(req, res, next) {
+exports.editPlaylist = function (req, res, next) {
   //console.log(req.body.playlistName);
   if (!req.body.playlistName) {
     return res.status(400).json({
-      error: "No playlist name provided."
+      error: "No playlist name provided.",
     });
   }
-  User.findById(req.user.id).then(user => {
+  User.findById(req.user.id).then((user) => {
     for (let i = 0; i < user.playlists.length; i++) {
       if (user.playlists[i]._id === req.params.id) {
         user.playlists[i].name = req.body.playlistName;
@@ -161,6 +161,6 @@ exports.editPlaylist = function(req, res, next) {
       }
     }
     user.markModified("playlists"); //very important.....
-    user.save().then(playlists => res.json(playlists));
+    user.save();
   });
 };
