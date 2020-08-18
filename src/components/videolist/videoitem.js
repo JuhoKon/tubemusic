9f850759de282af2b9ab695450ef11c2b50a542b
Juho Kontiainen <juho.kontiainen@visma.com>
2020-08-18 13:06:14 +0300

    Error handling

--- src/components/videolist/videoitem.js
+++ src/components/videolist/videoitem.js
@@ -233,6 +233,95 @@ class Videoitem extends Component {
               </CardBody>
             </Card>
           );
+        default:
+          return (
+            <Card
+              className="card-2"
+              id="videoitem"
+              onDoubleClick={this.onPlayClick.bind(this, this.props)}
+            >
+              <CardBody>
+                <Row>
+                  <Col xs="2" sm="2">
+                    <div
+                      className="thumbnailbutton"
+                      onClick={this.onPlayClick.bind(this, this.props)}
+                    >
+                      {this.props.thumbnail && (
+                        <LazyLoadImage
+                          height={70}
+                          src={this.props.thumbnail} // use normal <img> attributes as props
+                          width={70}
+                          style={{ position: "absolute" }}
+                          id="thumbnail"
+                        />
+                      )}
+                      <FontAwesomeIcon
+                        className="Active"
+                        size={"lg"}
+                        icon={faPlay}
+                        id="thumbnail2"
+                      />
+                      {/*     <Button
+                      className="btn btn-secondary float-right btn-item"
+                      onClick={this.onPlayClick.bind(this, this.props)}
+                    >
+                      Play
+                    </Button> */}
+                    </div>
+                  </Col>
+                  <Col xs="7" sm="7">
+                    <CardText>
+                      {this.props.title} -{" "}
+                      {this.props.artists[0] && (
+                        <RenderArtists artists={this.props.artists} />
+                      )}
+                    </CardText>
+                  </Col>
+                  <Col xs="2" sm="2">
+                    <br />
+                    <br />
+                    <div className="placeforbutton">
+                      <Button
+                        className="btn btn-primary btn-item"
+                        color="secondary"
+                        onClick={this.onAddToPlaylist.bind(this, this.props)}
+                      >
+                        +P
+                      </Button>
+                    </div>
+                  </Col>
+                  <Col xs="1" sm="1">
+                    <div className="placeforbutton">
+                      <Button
+                        className="btn btn-secondary  float-right btn-item"
+                        color="secondary"
+                        onClick={this.playNextClick.bind(this, this.props)}
+                      >
+                        +N
+                      </Button>
+                      <Button
+                        className="btn btn-secondary  float-right btn-item"
+                        color="secondary"
+                        onClick={this.onAddClick.bind(this, this.props)}
+                      >
+                        +Q
+                      </Button>
+                    </div>
+                  </Col>
+                </Row>
+
+                <Row>
+                  <Col xs="12" sm="12">
+                    <small className="float">
+                      {/*     Length&nbsp; */}
+                      {this.props.duration}
+                    </small>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          );
       }
     };
     //TODO: make it look much better
